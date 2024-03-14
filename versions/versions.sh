c6aaefc3e29255f968675fb0feb64e86068c0a9f
Maurice J Niessen <post@mjniessen.com>
2024-03-14 08:32:33 +0100

    versions: csv-head added & additional vars

--- versions/versions.sh
+++ versions/versions.sh
@@ -1,7 +1,22 @@
 #!/usr/bin/env bash
 
-while read -ra ARR; do
-	# skip lines not starting with 'ii'
-	[[ "${ARR[0]}" != "ii" ]] && continue
-	echo "${ARR[1]} ${ARR[2]}"
-done <<<"$(dpkg -l)"
+print_csv_head() {
+	echo -e "package\tversion\tarchitecture\tdescription"
+}
+
+parse_dpkg() {
+	while read -ra ARR; do
+		# skip lines not starting with 'ii'
+		[[ "${ARR[0]}" != "ii" ]] && continue
+
+		local package="${ARR[1]}"
+		local version="${ARR[2]}"
+		local architecture="${ARR[3]}"
+		local description="${ARR[*]:4}"
+
+		echo -e "${package}\t${version}\t${architecture}\t${description@Q}"
+	done <<<"$(dpkg -l)"
+}
+
+print_csv_head
+parse_dpkg
