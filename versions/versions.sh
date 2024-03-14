307b42df4b1eab63887e2cd05a362908813a0635
Maurice J Niessen <post@mjniessen.com>
2024-03-14 08:34:37 +0100

    versions: tabs as seperator

--- versions/versions.sh
+++ versions/versions.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+# Fail on errors (-e), undefined variables (-u) and single failed commands
+# within a pipeline (-o pipefail)
+set -euo pipefail
+
 print_csv_head() {
 	echo -e "package\tversion\tarchitecture\tdescription"
 }
@@ -18,5 +22,26 @@ parse_dpkg() {
 	done <<<"$(dpkg -l)"
 }
 
-print_csv_head
-parse_dpkg
+main() {
+  # Save possiple given arguments
+  ARGS=("$@")
+
+  OUTFILE="installed_software.csv"
+
+  #
+  # Your code has to be placed here.
+  #
+
+  print_csv_head > "${OUTFILE}"
+  parse_dpkg >> "${OUTFILE}"
+
+  exit 0
+}
+
+# Invoke main with args if not sourced
+# Approach via: https://stackoverflow.com/a/28776166/8787985
+if ! (return 0 2> /dev/null); then
+    main "$@"
+fi
+
+# vim: ft=bash ts=2 sw=2 sts=2 fileencoding=utf-8 foldmethod=marker
